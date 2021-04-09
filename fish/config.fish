0b31b83e628423438989c133136c97a2044ea56f
Kenji Aoki <ka@glia-computing.com>
2021-04-09 14:51:57 +0900

    fish の設定

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+alias ls='ls -G'
+alias rm='rm -i'
+alias mv='mv -i'
+alias cp='cp -i'
+alias less='less -iMR'
+alias vi='vim'
+alias irb='irb --noecho'
+alias ssh='ssh -o ServerAliveInterval=60'
+alias ccolumn="sed \"s/,/,|/g\" | column -ts ,"
+alias tcolumn="sed \"s/\\t/\\t|/g\" | column -t"
+
+set -x PAGER less
+set -x PATH $HOME/.local/bin $HOME/.cargo/bin $PATH
+set -x LC_ALL ja_JP.UTF-8
--- locate.sh
+++ locate.sh
@@ -17,3 +17,16 @@ do
   echo $FILE
   ln -s $CD/$FILE $HOME/.$FILE
 done
+
+FISH_DIR=$HOME/.config/fish
+mkdir -p $FISH_DIR
+
+FILES="
+config.fish
+fish_plugins
+"
+for FILE in $FILES
+do
+  echo $FILE
+  ln -s $CD/fish/$FILE $FISH_DIR/$FILE
+done
